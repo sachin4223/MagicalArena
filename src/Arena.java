dd0ee48d83b85afd47f5eb97a3b75f8476d0681d
Sachin <sachin.kumar1@nowfloats.com>
2024-07-10 15:25:32 +0530

    Add fight logic and turn execution in Arena class.

--- src/Arena.java
+++ src/Arena.java
@@ -8,4 +8,45 @@ public class Arena {
         this.playerB = playerB;
         this.dice = new Dice();
     }
+
+    public void fight() {
+        Player attacker, defender;
+
+        if (playerA.getHealth() < playerB.getHealth()) {
+            attacker = playerA;
+            defender = playerB;
+        } else {
+            attacker = playerB;
+            defender = playerA;
+        }
+
+        while (playerA.isAlive() && playerB.isAlive()) {
+            executeTurn(attacker, defender);
+            Player temp = attacker;
+            attacker = defender;
+            defender = temp;
+        }
+
+        if (playerA.isAlive()) {
+            System.out.println("Player A wins!");
+        } else {
+            System.out.println("Player B wins!");
+        }
+    }
+
+    private void executeTurn(Player attacker, Player defender) {
+        int attackRoll = dice.roll();
+        int defendRoll = dice.roll();
+
+        int attackDamage = attacker.attack(attackRoll);
+        int defendDamage = defender.defend(defendRoll);
+
+        int damageToDefender = attackDamage - defendDamage;
+        if (damageToDefender > 0) {
+            defender.takeDamage(damageToDefender);
+        }
+
+        System.out.printf("Attacker rolls: %d, Defender rolls: %d, Damage to Defender: %d, Defender's Health: %d%n",
+                attackRoll, defendRoll, Math.max(damageToDefender, 0), defender.getHealth());
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+// Main class to run the program
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Project setup complete.");
+        AttackStrategy attackStrategy = new StrengthBasedAttackStrategy();
+        DefenseStrategy defenseStrategy = new StrengthBasedDefenseStrategy();
+
+        Player playerA = new Player(50, 5, 10, attackStrategy, defenseStrategy);
+        Player playerB = new Player(10, 10, 5, attackStrategy, defenseStrategy);
+
+        Arena arena = new Arena(playerA, playerB);
+        arena.fight();
     }
 }
